1fdefed18bb9d22f4c5326541a30ca29382fc454
Bipin87 <sahubipin484@gmail.com>
2025-03-06 16:48:33 +0530

    [Bipin Kumar Sahu] Add: Validate the user name using input events added

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   imgUrl = "BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
   username: String="";
+  nameError: String="";
 
   ngOnInit():void{
     this.title = "Hello From BridgeLabz."
@@ -20,5 +21,14 @@ export class AppComponent {
   onClick($event:Event){
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+  onInput($event:any){
+    console.log("change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+    if(nameRegex.test(this.username.toString())){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!";
   }
 }
